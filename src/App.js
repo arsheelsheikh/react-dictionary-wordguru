6907a43c8627bea3ec181ee634309c2be20f9c74
arsheel <sheikharsheel@gmail.com>
2023-07-24 22:24:08 +0530

    Added Header component and API language options

--- src/App.js
+++ src/App.js
@@ -8,10 +8,13 @@ function App() {
 
   const [word, setWord] = useState("");
   const [meanings, setMeanings] = useState({});
+  const [category, setCategory] = useState("en");
 
   const dictionaryApi = async() => {
     try {
-      const data = await axios.get('https://api.dictionaryapi.dev/api/v2/entries/en/plane');
+      const data = await axios.get(
+        `https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`
+        );
 
       
       setMeanings(data.data);
@@ -25,11 +28,15 @@ function App() {
 
   useEffect(() => {
     dictionaryApi();
-  }, []);
+  }, [word, category]);
 
   return ( <div className="App" style={{height:'100vh', backgroundColor:"#282c34", color:"white"}}>
     <Container maxWidth="md" style={{display:"flex", flexDirection: "column", height:'100vh'}}>
-      <Header />
+      <Header 
+        category={category} 
+        setCategory={setCategory} 
+        word={word} 
+        setWord={setWord} />
     </Container>
   </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,54 @@
 import React from 'react';
 import "./Header.css";
+import categories from '../data/category';
+import MenuItem from "@material-ui/core/MenuItem";
+import { createMuiTheme, TextField, ThemeProvider } from "@material-ui/core";
 
-const Header = () => {
-  return (
-    <div className='header'>
-        <span className='title'>Word Guru</span>
-        <div className='inputs'>
-            hello
+const Header = ({setCategory, category, word, setWord}) => {
+    const darkTheme = createMuiTheme({
+        palette: {
+          primary: {
+            main: "#fff",
+          },
+          type: "dark",
+        },
+      });
+
+
+      const handleChange = () => {
+
+      }
+
+
+      return (
+        <div className="header">
+          <span className="title">{word ? word : "Word Hunt"}</span>
+          <div className="inputs">
+            <ThemeProvider theme={darkTheme}>
+              <TextField
+                className="search"
+                label="Search a Word"
+                id="filled-basic"
+                value={word}
+                onChange={(e) => setWord(e.target.value)}
+              />
+              <TextField
+                className="select"
+                select
+                label="Language"
+                value={category}
+                onChange={(e) => handleChange(e.target.value)}
+              >
+                {categories.map((option) => (
+                  <MenuItem key={option.label} value={option.label}>
+                    {option.value}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </ThemeProvider>
+          </div>
         </div>
-    </div>
-  )
-}
+      );
+    };
 
-export default Header
+export default Header;
